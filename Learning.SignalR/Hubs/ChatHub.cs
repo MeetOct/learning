efaae677f83e281f330e3769b17f4b5629119ee8
Unknown <302821616@qq.com>
2016-09-27 17:34:24 +0800

    signalr chat hub

---
+++ Learning.SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Learning.SignalR.Hubs
+{
+	[HubName("chatHub")]
+	public class ChatHub : Hub
+	{
+		public void Hello()
+		{
+			Clients.All.hello();
+		}
+
+		public void Say(string message)
+		{
+			Clients.All.say(message);
+		}
+	}
+}
